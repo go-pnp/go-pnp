4077598d7b389f71e75610acd853775379467b18
Yaroslav <saturn4er@gmail.com>
2024-09-17 03:34:01 +0300

    Add pnphealthcheckgorm

---
+++ healthcheck/pnphealthcheckgorm/go.mod
@@ -0,0 +1,23 @@
+module github.com/go-pnp/go-pnp/healthcheck/pnphealthcheckgorm
+
+go 1.22
+
+require (
+	github.com/go-pnp/go-pnp v1.0.0
+	github.com/go-pnp/go-pnp/healthcheck/pnphealthcheck v0.0.5
+	go.uber.org/fx v1.22.2
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	github.com/go-pnp/jobber v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	go.uber.org/dig v1.18.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ healthcheck/pnphealthcheckgorm/module.go
@@ -0,0 +1,42 @@
+package pnphealthchecksql
+
+import (
+	"context"
+
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+
+	"github.com/go-pnp/go-pnp/fxutil"
+	"github.com/go-pnp/go-pnp/healthcheck/pnphealthcheck"
+	"github.com/go-pnp/go-pnp/pkg/optionutil"
+)
+
+func Module(opts ...optionutil.Option[options]) fx.Option {
+	options := newOptions(opts)
+
+	moduleBuilder := &fxutil.OptionsBuilder{
+		PrivateProvides: options.fxPrivate,
+	}
+	moduleBuilder.Supply(options)
+	moduleBuilder.Provide(pnphealthcheck.HealthCheckerProvider(NewHealthChecker))
+
+	return moduleBuilder.Build()
+}
+
+func NewHealthChecker(
+	db *gorm.DB,
+	options *options,
+) pnphealthcheck.HealthChecker {
+	return pnphealthcheck.HealthChecker{
+		Name:    options.name,
+		Timeout: options.timeout,
+		Check: func(ctx context.Context) error {
+			sqlDB, err := db.DB()
+			if err != nil {
+				return err
+			}
+
+			return sqlDB.PingContext(ctx)
+		},
+	}
+}
