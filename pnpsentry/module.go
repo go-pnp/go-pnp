7c0bfeefd6182fae001c84380be43d845e3d00e9
Yaroslav <saturn4er@gmail.com>
2025-01-15 02:26:03 +0200

    Fix pnpsentry

--- pnpsentry/module.go
+++ pnpsentry/module.go
@@ -12,6 +12,7 @@ func Module(opts ...optionutil.Option[options]) fx.Option {
 	options := newOptions(opts)
 
 	builder := fxutil.OptionsBuilder{}
+	builder.Supply(options)
 	builder.ProvideIf(!options.configFromContainer, configutil.NewPrefixedConfigProvider[Config](options.configPrefix))
 	builder.PublicProvideIf(!options.configFromContainer, configutil.NewPrefixedConfigInfoProvider[Config](options.configPrefix))
 	builder.Provide(fx.Annotate(NewSentryClient, fx.OnStop(CloseClient)))
