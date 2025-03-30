8fc8ba0ebcfd8a1274178775e38c9a7ba3bcd419
Yaroslav <saturn4er@gmail.com>
2025-03-30 19:23:06 +0300

    add constLabels option to pnppromconnectrpchandling

--- connectrpc/pnppromconnectrpchandling/options.go
+++ connectrpc/pnppromconnectrpchandling/options.go
@@ -2,13 +2,15 @@ package pnppromconnectrpchandling
 
 import (
 	"github.com/go-pnp/go-pnp/pkg/optionutil"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type options struct {
-	fxPrivate bool
-	namespace string
-	subsystem string
-	order     int
+	fxPrivate   bool
+	namespace   string
+	subsystem   string
+	order       int
+	constLabels prometheus.Labels
 }
 
 // WithFxPrivate is an option to add fx.Private to all module provides.
@@ -21,5 +23,11 @@ func WithFxPrivate() optionutil.Option[options] {
 func WithOrder(order int) optionutil.Option[options] {
 	return func(o *options) {
 		o.order = order
+	}
+}
+
+func WithConstLabels(constLabels prometheus.Labels) optionutil.Option[options] {
+	return func(o *options) {
+		o.constLabels = constLabels
 	}
 }
