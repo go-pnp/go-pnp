0ab7eecc83d0bae58803eb8a026401fa6690fb5a
VB <250989417+vb-sklr@users.noreply.github.com>
2026-02-05 19:08:15 +0200

    feat: introduce job decorator support and simplify worker module setup

--- pnpjobber/workers.go
+++ pnpjobber/workers.go
@@ -2,41 +2,42 @@ package pnpjobber
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/go-pnp/jobber"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/trace"
-	"go.uber.org/fx"
 
 	"github.com/go-pnp/go-pnp/logging"
+	"github.com/go-pnp/go-pnp/pkg/ordering"
+	"github.com/go-pnp/jobber"
+	"go.uber.org/fx"
 )
 
 type invokeJobberRunnerParams struct {
 	fx.In
-	Lc         fx.Lifecycle
-	Shutdowner fx.Shutdowner
-	Job        jobber.Job
-	Tracer     trace.Tracer
-	Logger     *logging.Logger `optional:"true"`
+
+	Lc            fx.Lifecycle
+	Shutdowner    fx.Shutdowner
+	Job           jobber.Job
+	JobDecorators ordering.OrderedItems[JobDecorator] `group:"pnp_jobber.job_decorators"`
+	Logger        *logging.Logger                     `optional:"true"`
 }
 
 // Module runs worker provided via jobProvider,
 // One of the result of jobProvider should be jobber.Job
-func Module(workerName string, workerProvider any, workerOptions ...jobber.OptionFunc) fx.Option {
+func Module(workerProvider any, workerOptions ...jobber.OptionFunc) fx.Option {
 	return fx.Module(
-		workerName,
+		"job",
 		fx.Provide(
 			workerProvider,
 			fx.Private,
 		),
-		logging.DecorateNamed(fmt.Sprintf("%s_worker", workerName)),
+		decorateLoggerWithJobName(),
+
 		fx.Invoke(func(params invokeJobberRunnerParams) {
-			opts := []jobber.OptionFunc{
-				jobber.WithMiddlewares(traceMiddleware(params.Tracer, workerName)),
+			job := params.Job
+			for _, decorator := range params.JobDecorators.Get() {
+				job = decorator(job)
 			}
-			opts = append(opts, workerOptions...)
-			workerRunner := jobber.NewRunner(params.Job, opts...)
+
+			workerRunner := jobber.NewRunner(job, workerOptions...)
+
 			params.Lc.Append(fx.Hook{
 				OnStart: func(ctx context.Context) error {
 					params.Logger.Info(ctx, "Starting worker")
@@ -61,17 +62,4 @@ func Module(workerName string, workerProvider any, workerOptions ...jobber.Optio
 			})
 		}),
 	)
-}
-
-func traceMiddleware(tracer trace.Tracer, workerName string) jobber.Middleware {
-	return func(next jobber.HandleFunc) jobber.HandleFunc {
-		return func(ctx context.Context) error {
-			ctx, span := tracer.Start(ctx, "job "+workerName)
-			defer span.End()
-
-			span.SetAttributes(attribute.String("job.name", workerName))
-
-			return next(ctx)
-		}
-	}
 }
